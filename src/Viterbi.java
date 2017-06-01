92a57f59925dd1d5dce674e2ff384aec8b87d6a6
João Gehlen <joaogehlen91@gmail.com>
2017-05-31 21:46:06 -0300

    feito o ruido

--- src/Viterbi.java
+++ src/Viterbi.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Viterbi {
 
@@ -11,7 +14,8 @@ public class Viterbi {
 		String estadoAtual = "00";
 		String saida = "";
 		
-		String entrada = "10101";
+		String entrada = "11010111011011";
+		Double ruido = 0.2; // nivel de ruido em porcentagem(0 - 1), exemplo: 0.75 = 75% de ruido
 		
 		System.out.println("Est Atu |Ent |Emite |Prox Est");
 		
@@ -32,16 +36,73 @@ public class Viterbi {
 		 * Além disto são gerados 2 pares de bits extras no final para usar toda a influencia dos bits de entrada.
 		 * Nao lembro como que é esses dois bits, repete os dois ultimos? o ultimo duas vezes??
 		 * */
-		System.out.println("\nSaida: " + saida);
 		
-		/* TODO
-		 * fazer ruido
-		 * 
-		 * */
+		
+		
+		System.out.println("\nSaida:           " + saida);
+		
+		/* INICIO RUIDO */
+		// calcula a quantidade de bits que precisa ser trocado
+		int qtdBitsRuido = (int) Math.round((saida.length() * ruido));
+		
+		// cria lista para colocar a saida com ruido
+		ArrayList<String> bitsSaidaComRuido = new ArrayList<String>();
+		bitsSaidaComRuido = converteStringtoArrayList(saida);
+		
+		// lista usada para sortear os bits a serem trocados
+		ArrayList<Integer> listIDs = new ArrayList<Integer>();
+        for (int i = 0; i < bitsSaidaComRuido.size(); i++) listIDs.add(new Integer(i));
+        Collections.shuffle(listIDs);
+				
+        // laço para realizar a troca dos bits sorteados;
+		for (int i = 0; i < qtdBitsRuido; i++) {
+			Integer idBit = listIDs.get(0);
+			listIDs.remove(0);
+			//System.out.println("Troca o bit: " + idBit);
+			bitsSaidaComRuido.set(idBit, inverte(bitsSaidaComRuido.get(idBit)));
+		}
+		
+		// formata a saida com os bits ja trocados
+		String saidaComRuido = "";
+		for (String bit : bitsSaidaComRuido) saidaComRuido = saidaComRuido + bit;
+		
+		System.out.println("Qtd trocado: " + qtdBitsRuido);
+		System.out.println("Saida com Ruido: " + saidaComRuido);
+		/* FIM RUIDO */
+		
+		
+		
+		
 	}
 
 	
 	
+	
+
+
+
+	private static ArrayList<String> converteStringtoArrayList(String texto) {
+		char[] caracteres = texto.toCharArray();
+		ArrayList<String> lista = new ArrayList<String>();
+		for(char c : caracteres){
+		    lista.add(String.valueOf(c));
+		}
+		return lista;
+	}
+
+
+
+
+
+
+
+	private static String inverte(String string) {
+		if (string.equals("0")) return "1";
+		return "0";
+	}
+
+
+
 	private static int stringParaInt(String string) {
 		int saida = 0;
 		switch (string) {
